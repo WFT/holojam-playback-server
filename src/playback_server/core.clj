6c6e6f990b0158e0ed7f22f98a1ba4ba66856fcc
WFT <will.a.ft@gmail.com>
2015-10-13 17:41:21 -0500

    separating out routes.clj

--- src/playback_server/core.clj
+++ src/playback_server/core.clj
@@ -1,35 +1,8 @@
 (ns playback-server.core
-  (:require [playback-server.handlers :refer :all]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer
-             [wrap-defaults api-defaults]]
-            [ring.middleware.resource :refer
-             [wrap-resource]]
-            [ring.util.response :as response]
+  (:require [playback-server.routes :as routes]
             [aleph.http :as http]
-            [clojure.java.shell :refer [sh]]
-            [playback-server.frames :as frames])
+            [clojure.java.shell :refer [sh]])
   (:gen-class))
-
-(def ^:dynamic frames-data-path
-  "recorded_frames")
-
-(defroutes app-routes
-  (GET "/frames" request
-       (read-frame-handler frames-data-path request))
-  (GET "/frames/count" []
-       (str (frames/get-frame-count frames-data-path)))
-  (GET "/frames/:fnum{[0-9]+}" [fnum]
-       (read-frame-http-handler frames-data-path fnum))
-  (GET "/" []
-       (response/redirect "/index.html"))
-  (route/not-found "404 Not Found"))
-
-(def app
-  (-> #'app-routes
-      (wrap-resource "unity")
-      (wrap-defaults api-defaults)))
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (first args) "8080"))]
@@ -44,6 +17,6 @@
             (println (:err proc))))
         (catch java.io.IOException e
           (println "No python installation; couldn't open webbrowser."))))
-    (binding [frames-data-path (or (second args) frames-data-path)]
-      (http/start-server #'app {:port port}))
+    (binding [routes/frames-data-path (or (second args) routes/frames-data-path)]
+      (http/start-server #'routes/app {:port port}))
     (shutdown-agents)))
---
+++ src/playback_server/routes.clj
@@ -0,0 +1,30 @@
+(ns playback-server.routes
+  (:require [playback-server.handlers :refer :all]
+            [playback-server.frames :as frames]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer
+             [wrap-defaults api-defaults]]
+            [ring.middleware.resource :refer
+             [wrap-resource]]
+            [ring.util.response :as response]))
+
+(def ^:dynamic frames-data-path
+  "recorded_frames")
+
+(defroutes app-routes
+  (GET "/frames" request
+       (read-frame-handler frames-data-path request))
+  (GET "/frames/count" []
+       (str (frames/get-frame-count frames-data-path)))
+  (GET "/frames/:fnum{[0-9]+}" [fnum]
+       (read-frame-http-handler frames-data-path fnum))
+  (GET "/" []
+       (response/redirect "/index.html"))
+  (route/not-found "404 Not Found"))
+
+(def app
+  (-> #'app-routes
+      (wrap-resource "unity")
+      (wrap-defaults api-defaults)))
+
